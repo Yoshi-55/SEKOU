d9385326192635a6a89a71180844affec8317fbd
Yoshi-55 <ysmr9855@gmail.com>
2026-02-16 22:19:48 +0900

    テスト整理: 空のスケルトンファイルを全削除

--- spec/views/applies/accept.html.tailwindcss_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe 'applies/accept.html.tailwindcss', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end

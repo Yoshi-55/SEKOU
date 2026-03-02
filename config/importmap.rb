ef9bf745c2875143cb8b72d900ac7290329c6185
Yoshi-55 <ysmr9855@gmail.com>
2026-03-02 10:59:02 +0900

    importmapにjob_formを追加

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'application'
+pin 'job_form', to: 'job_form.js'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
